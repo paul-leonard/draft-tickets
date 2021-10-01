8dcc067195b50586e9e908abf00fa8e342071229
Paul Leonard <paul.ryan.leonard@gmail.com>
2021-10-01 16:43:36 -0700

    login form fields created

--- forms/LoginForm.js
+++ forms/LoginForm.js
@@ -4,12 +4,45 @@ import SubmitButton from "../components/SubmitButton";
 import Footer from "../components/Footer";
 import Router from 'next/router';
 import axios from "axios";
+import InputField from "../components/InputField";
 
-export default function LoginForm () {
-  return (
-    <>
-      <h1>Form TBD</h1>
-      <SubmitButton/>
-    </>
-  )
+class LoginForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username:'',
+      password:'',
+      buttonDisabled: false,
+      isAuthenticated: false
+    }
+  }
+  render() {
+    return(
+      <div className="signInPage">
+        <h1>Log In</h1>
+
+        <InputField
+          type = 'text'
+          placeholder = 'username'
+          value = {this.state.username ? this.state.username : ''}
+          onChange = {(val) => this.setInputValue('username',val)}
+        />
+
+        <InputField
+          type = 'password'
+          placeholder = 'password'
+          value = {this.state.password ? this.state.password : ''}
+          onChange = {(val) => this.setInputValue('password',val)}
+        />
+
+        <SubmitButton
+          text = 'Login'
+          disabled = {this.state.buttonDisabled}
+          onClick = {() => this.doSignIn()}  
+        />
+      </div>
+    )
+  }
 }
+
+export default LoginForm;
